fdf3751fc4fe590e080547cc8b9f4ba8a13f7c5f
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-14 18:33:02 +0900

    #7. 포스팅 목록 API에 JWT 인증

--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+from time import timezone
+
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
-
 from .models import Post
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
@@ -10,4 +13,15 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()  # DB의 전체에 대해서 쿼리를 날립니다.
     serializer_class = PostSerializer
-    permission_classes = [AllowAny]  # FIXME: 인증 적용
+    # permission_classes = [AllowAny]  # FIXME: 인증 적용
+
+    def get_queryset(self):
+        timesince = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        qs = qs.filter(
+            Q(author=self.request.user)
+            | Q(author__in=self.request.user.following_set.all())
+        )
+        qs = qs.filter(created_at__gte=timesince)
+
+        return qs
--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import Post from "./Post";
 import { useAppContext } from "../store";
+import { Alert } from "antd";
 
 const apiUrl = "http://localhost:8000/api/posts/";
 
 function PostList() {
-  // const {
-  //   store: { jwtToken },
-  //   dispatch,
-  // } = useAppContext();
+  const {
+    store: { jwtToken },
+    dispatch,
+  } = useAppContext();
   const [postList, setPostList] = useState([]);
 
   useEffect(() => {
-    Axios.get(apiUrl)
+    const headers = { Authorization: `JWT ${jwtToken}` };
+    Axios.get(apiUrl, { headers })
       .then((response) => {
         const { data } = response;
         console.log("loaded response :", response);
@@ -27,6 +29,9 @@ function PostList() {
 
   return (
     <div>
+      {postList.length === 0 && (
+        <Alert type="warning" message="포스팅이 없습니다." />
+      )}
       {postList.map((post) => (
         <Post post={post} key={post.id} />
       ))}
