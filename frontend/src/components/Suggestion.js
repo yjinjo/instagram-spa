3acef154f7e42c146c8f44e70cdd8ef4d5f3673a
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-14 20:02:34 +0900

    #7. 유저 Follow 리액트 화면 구현

--- frontend/src/components/Suggestion.js
+++ frontend/src/components/Suggestion.js
@@ -3,8 +3,8 @@ import { Avatar, Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import "./Suggestion.scss";
 
-function Suggestion({ suggestionUser }) {
-  const { username, name, avatar_url } = suggestionUser;
+function Suggestion({ suggestionUser, onFollowUser }) {
+  const { username, name, avatar_url, is_follow } = suggestionUser;
 
   return (
     <div className="suggestion">
@@ -22,7 +22,12 @@ function Suggestion({ suggestionUser }) {
       </div>
       <div className="username">{name.length === 0 ? username : name}</div>
       <div className="action">
-        <Button size="small">Follow</Button>
+        {is_follow && "팔로잉 중"}
+        {!is_follow && (
+          <Button size="small" onClick={() => onFollowUser(username)}>
+            Follow
+          </Button>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -11,12 +11,28 @@ function SuggestionList({ style }) {
     store: { jwtToken },
   } = useAppContext();
 
+  const [userList, setUserList] = useState([]);
+
   const headers = { Authorization: `JWT ${jwtToken}` };
 
-  const [{ data: userList, loading, error }, refetch] = useAxios({
+  const [{ data: originUserList, loading, error }, refetch] = useAxios({
     url: "http://localhost:8000/accounts/suggestions/",
     headers,
   });
+
+  useEffect(() => {
+    if (!originUserList) setUserList([]);
+    else
+      setUserList(originUserList.map((user) => ({ ...user, follow: false })));
+  }, [originUserList]);
+
+  const onFollowUser = (username) => {
+    setUserList((prevUserList) =>
+      prevUserList.map((user) =>
+        user.username !== username ? user : { ...user, is_follow: true }
+      )
+    );
+  };
 
   return (
     <div style={style}>
@@ -24,13 +40,13 @@ function SuggestionList({ style }) {
       {error && <div>로딩 중에 에러가 발생했습니다.</div>}
 
       <Card title="Suggestions for you " size="small">
-        {userList &&
-          userList.map((suggestionUser) => (
-            <Suggestion
-              key={suggestionUser.username}
-              suggestionUser={suggestionUser}
-            />
-          ))}
+        {userList.map((suggestionUser) => (
+          <Suggestion
+            key={suggestionUser.username}
+            suggestionUser={suggestionUser}
+            onFollowUser={onFollowUser}
+          />
+        ))}
       </Card>
     </div>
   );
