89cde786dc4d098065da90ee5a5e185281dc5559
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-14 19:04:15 +0900

    #7. Suggestion 목록 화면 구현

--- frontend/src/components/Suggestion.js
+++ frontend/src/components/Suggestion.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Button } from "antd";
+import { Avatar, Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import "./Suggestion.scss";
 
-function Suggestion() {
+function Suggestion({ suggestionUser }) {
+  const { username, name, avatar_url } = suggestionUser;
+
   return (
     <div className="suggestion">
       <div className="avatar">
-        <UserOutlined />
+        <Avatar
+          size="small"
+          icon={
+            <img
+              src={"http://localhost:8000" + avatar_url}
+              alt={`${username}'s avatar`}
+            />
+          }
+        />
+        {/*<UserOutlined />*/}
       </div>
-      <div className="username">Username</div>
+      <div className="username">{name.length === 0 ? username : name}</div>
       <div className="action">
         <Button size="small">Follow</Button>
       </div>
--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "antd";
 import "./SuggestionList.scss";
 import Suggestion from "./Suggestion";
+import Axios from "axios";
+import { useAppContext } from "../store";
 
 function SuggestionList({ style }) {
+  const {
+    store: { jwtToken },
+  } = useAppContext();
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserList() {
+      const apiUrl = "http://localhost:8000/accounts/suggestions/";
+      const headers = { Authorization: `JWT ${jwtToken}` };
+      try {
+        const { data } = await Axios.get(apiUrl, { headers });
+        setUserList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchUserList();
+  }, []);
   return (
     <div style={style}>
-      <Card title="Suggestions for you" size="small">
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
+      <Card title="Suggestions for you " size="small">
+        {userList.map((suggestionUser) => (
+          <Suggestion
+            key={suggestionUser.username}
+            suggestionUser={suggestionUser}
+          />
+        ))}
       </Card>
     </div>
   );
