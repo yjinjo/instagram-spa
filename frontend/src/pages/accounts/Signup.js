b586d3fd91d084843732f01d7b192ef5d0565d0d
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-14 17:07:43 +0900

    #6. 로그인 폼을 만들고 로컬 스토리지에 jwt 저장

--- frontend/src/pages/accounts/Signup.js
+++ frontend/src/pages/accounts/Signup.js
@@ -70,6 +70,9 @@ function Signup() {
           { required: true, message: "Please input your username!" },
           { min: 5, message: "5글자 입력해주세요." },
         ]}
+        hasFeedback
+        {...fieldErrors.username}
+        {...fieldErrors.non_field_errors}
       >
         <Input />
       </Form.Item>
@@ -78,6 +81,7 @@ function Signup() {
         label="Password"
         name="password"
         rules={[{ required: true, message: "Please input your password!" }]}
+        {...fieldErrors.password}
       >
         <Input.Password />
       </Form.Item>
@@ -94,10 +98,6 @@ function Signup() {
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
-};
-
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
 };
 
 export default Signup;
