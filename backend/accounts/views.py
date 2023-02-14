afe92c6bbedd9916f1b9939d6a4cd3cf01ebb029
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-15 07:53:57 +0900

    #7. 유저 Follow API 구현

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
 from rest_framework import permissions
+from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.response import Response
 from .serializers import SignupSerializer, SuggestionUserSerializer
 
 
 class SignUpView(CreateAPIView):
-    # User 모델이 바뀔 수 있기 때문에 바로 User 모델을 쓰는 것보다 get_user_model을 호출합니다.
+    # User 모델이 바뀔 수 있기 때문에 바로  User 모델을 쓰는 것보다 get_user_model을 호출합니다.
     model = get_user_model()
     serializer_class = SignupSerializer
 
@@ -18,3 +21,30 @@ class SignUpView(CreateAPIView):
 class SuggestionListAPIView(ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = SuggestionUserSerializer
+
+    def get_queryset(self):
+        qs = (
+            super()
+            .get_queryset()
+            .exclude(pk=self.request.user.pk)
+            .exclude(pk__in=self.request.user.following_set)
+        )
+        return qs
+
+
+@api_view(["POST"])
+def user_follow(request):
+    username = request.data["username"]
+    follow_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    request.user.following_set.add(follow_user)
+    follow_user.follower_set.add(request.user)
+    return Response(status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["POST"])
+def user_unfollow(request):
+    username = request.data["username"]
+    follow_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    request.user.following_set.remove(follow_user)
+    follow_user.follower_set.remove(request.user)
+    return Response(status.HTTP_204_NO_CONTENT)
--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -27,11 +27,19 @@ function SuggestionList({ style }) {
   }, [originUserList]);
 
   const onFollowUser = (username) => {
-    setUserList((prevUserList) =>
-      prevUserList.map((user) =>
-        user.username !== username ? user : { ...user, is_follow: true }
-      )
-    );
+    const data = { username };
+    const config = { headers };
+    Axios.post("http://localhost:8000/accounts/follow/", data, config)
+      .then((response) => {
+        setUserList((prevUserList) =>
+          prevUserList.map((user) =>
+            user.username !== username ? user : { ...user, is_follow: true }
+          )
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
